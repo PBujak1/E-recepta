cdf17598e748ac3a7462c8f73fbd3d1c0a16d0a0
PPPita <1piotrbujak@gmail.com>
2026-01-10 14:23:10 +0100

    Update

--- src/main/java/com/example/erecepta/stworzKontoPac.java
+++ src/main/java/com/example/erecepta/stworzKontoPac.java
@@ -1,8 +1,19 @@
 package com.example.erecepta;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class stworzKontoPac {
     public void start(Stage primaryStage) {
-    }
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 1300, 780); //1300, 780
+        scene.getStylesheets().add(
+                getClass().getResource("/css/mainPanels/styleLek.css").toExternalForm()
+        );
+
+        primaryStage.setTitle("E-Recepta");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    };
 }
