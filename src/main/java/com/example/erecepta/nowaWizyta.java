c4c8b683e408f94602240b7bd1667cdd5b8bebbe
PPPita <1piotrbujak@gmail.com>
2026-03-06 19:46:42 +0100

    Update

--- src/main/java/com/example/erecepta/nowaWizyta.java
+++ src/main/java/com/example/erecepta/nowaWizyta.java
@@ -2,53 +2,58 @@ package com.example.erecepta;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
-import javafx.animation.Animation;
-import javafx.animation.Interpolator;
-import javafx.animation.RotateTransition;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
-import java.io.IOException;
+import java.util.Objects;
 
 public class nowaWizyta {
+    //Tytuł
+    private final Label titleLabel = new Label("Formularz E-wyzyty");
 
-    private FontAwesomeIconView searchIcon = new FontAwesomeIconView(FontAwesomeIcon.SEARCH);
-    private FontAwesomeIconView iconUser = new FontAwesomeIconView(FontAwesomeIcon.USER_CIRCLE);
+    //Top panel
+    private final FontAwesomeIconView searchIcon = new FontAwesomeIconView(FontAwesomeIcon.SEARCH);
+    private final TextField searchField = new TextField();
+    private final Button acceptBtn = new Button("Wybierz");
 
-    private ToggleButton stacjonarnieBtn = new ToggleButton("Stacjonarnie");
-    private ToggleButton Eporada = new ToggleButton("E-porada");
+    //Pod top panel
+    private final FontAwesomeIconView iconUser = new FontAwesomeIconView(FontAwesomeIcon.USER_CIRCLE);
+    private final ToggleButton stacjonarnieBtn = new ToggleButton("Stacjonarnie");
+    private final ToggleButton Eporada = new ToggleButton("E-porada");
+    private final Label rodzajString = new Label("Rodzaj wizyty");
 
-    private TextField searchField = new TextField();
-
-    private Button testButton = new Button("Test");
-    private Button exitButton = new Button("Wyjście");
+    //Guziczki
+    private final Button exitButton = new Button("Wyjście");
 
     public void start(Stage primaryStage){
         VBox root = new VBox();
         root.setAlignment(Pos.TOP_CENTER);
         root.getStyleClass().add("main-panel");
 
+        /*
+            Górny panel
+         */
         HBox topBox = new HBox(10);
         topBox.setAlignment(Pos.CENTER);
+        acceptBtn.getStyleClass().add("acceptBtn");
         topBox.getStyleClass().add("top-panel");
-        topBox.getChildren().addAll(searchIcon, searchField);
-
+        topBox.getChildren().addAll(searchIcon, searchField, acceptBtn);
         HBox.setHgrow(searchField, Priority.ALWAYS);
+        searchField.setPromptText("Wyszukaj lekarza");
+        searchField.getStyleClass().add("search-field");
 
-        iconUser.setGlyphSize(30);
-        iconUser.setStyleClass("my-user-icon");
-        searchIcon.setGlyphSize(30);
-        searchIcon.setStyleClass("my-search-icon");
-
-        HBox selection = new HBox();
-        selection.getStyleClass().add("selection");
+        /*
+            Panel do wybrania formy e-wizyty
+         */
+        VBox selectionBox = new VBox(15);
+        selectionBox.getStyleClass().add("selection");
+        HBox selection = new HBox(20);
         selection.setAlignment(Pos.CENTER);
 
         ToggleGroup toggleGroup = new ToggleGroup();
@@ -56,27 +61,54 @@ public class nowaWizyta {
         Eporada.setToggleGroup(toggleGroup);
         stacjonarnieBtn.setMaxWidth(Double.MAX_VALUE);
         Eporada.setMaxWidth(Double.MAX_VALUE);
-        stacjonarnieBtn.setSelected(true);
         HBox.setHgrow(stacjonarnieBtn, Priority.ALWAYS);
         HBox.setHgrow(Eporada, Priority.ALWAYS);
-        selection.getChildren().addAll(iconUser, stacjonarnieBtn, Eporada);
+        selection.getChildren().addAll(stacjonarnieBtn, Eporada);
+        selectionBox.getChildren().addAll(rodzajString, selection);
+
+        /*
+            Utworzenie panelu lekarza po jego wcześniejszym wyszukaniu
+         */
+        HBox lekarz = new HBox(10);
+        lekarz.setAlignment(Pos.CENTER_LEFT);
+        VBox daneLekarza = new VBox(10);
+        daneLekarza.setAlignment(Pos.CENTER_LEFT);
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        lekarz.getChildren().addAll(
+                iconUser,
+                spacer,
+                daneLekarza
+        );
+        lekarz.getStyleClass().add("lekarz");
 
         root.getChildren().addAll(
+                titleLabel,
                 topBox, new Separator(),
-                selection, new Separator(),
-                testButton,
+                selectionBox, new Separator(),
+                lekarz, new Separator(),
                 exitButton
         );
 
         Scene scene = new Scene(root, 1300, 780);
         scene.getStylesheets().add(
-                getClass().getResource("/css/mainPacPanels/nowaWizyta.css").toExternalForm()
+                Objects.requireNonNull(getClass().getResource("/css/mainPacPanels/nowaWizyta.css")).toExternalForm()
         );
 
         primaryStage.setTitle("E-Recepta");
         primaryStage.setScene(scene);
         primaryStage.show();
-    };
+
+        /*
+            Ustawienie wyglądu ikon
+         */
+        iconUser.setGlyphSize(60);
+        iconUser.setStyleClass("my-user-icon");
+        searchIcon.setGlyphSize(30);
+        searchIcon.setStyleClass("my-search-icon");
+        exitButton.getStyleClass().add("exit-btn");
+        titleLabel.getStyleClass().add("title-label");
+    }
 
     public Button getWyjdzButton() {
         return exitButton;
